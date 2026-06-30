4dcd5be0af42d239e12f94864a5e24dda9a40cef
Cetusone <qwerly76@gmail.com>
2026-06-30 14:16:49 +0300

    Набросана backend архитектура
    Выбрал модульный монолит. Он упростит разработку mvp, а также останется возможность декомпозировать модули на отдельные микросервисы

---
+++ backend/shulker-srm/src/test/java/com/cetus/shulkersrm/ShulkerSrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cetus.shulkersrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShulkerSrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
