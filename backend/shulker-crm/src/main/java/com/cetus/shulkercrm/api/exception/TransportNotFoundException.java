1421286e9e55d4db2e154b2a40c2ab93eb1d72e7
Cetusone <qwerly76@gmail.com>
2026-07-08 12:31:03 +0300

    Delete backend directory

--- backend/shulker-crm/src/main/java/com/cetus/shulkercrm/api/exception/TransportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cetus.shulkercrm.api.exception;
-
-public class TransportNotFoundException extends ResourceNotFoundException {
-    public TransportNotFoundException(Long id) {
-        super("Транспорт с ID " + id + " не найден");
-    }
-}
--- backend/shulker-crm/src/main/java/com/cetus/shulkercrm/logistics/internal/repository/OwnWarehouseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cetus.shulkercrm.logistics.internal.repository;
-
-import com.cetus.shulkercrm.logistics.internal.entity.OwnWarehouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnWarehouseRepository extends JpaRepository<OwnWarehouse, Long> {
-}
